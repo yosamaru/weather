de79fb20e7c2062d8305cbf4ac33796070e1a31e
Lei.W <lei@LeiWdeMacBook-Pro.local>
2020-09-28 08:20:14 +0800

    feat

---
+++ src/components/ResultFadeIn.js
@@ -0,0 +1,11 @@
+import { keyframes } from 'styled-components';
+// 淡入，淡出功能
+const ResultFadeIn = keyframes`
+  to {
+    opacity: 1;
+        visibility: visible;
+        top: 0;
+  }
+`;
+
+export default ResultFadeIn;
